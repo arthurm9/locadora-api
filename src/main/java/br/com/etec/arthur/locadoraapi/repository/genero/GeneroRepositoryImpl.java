70b140bda9ced1959ebcceefe2ea3d7db6029917
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-05-17 07:49:33 -0300

    entity manager stated
    with persistence context notation

--- src/main/java/br/com/etec/arthur/locadoraapi/repository/genero/GeneroRepositoryImpl.java
+++ src/main/java/br/com/etec/arthur/locadoraapi/repository/genero/GeneroRepositoryImpl.java
@@ -5,7 +5,14 @@ import br.com.etec.arthur.locadoraapi.repository.filter.GeneroFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class GeneroRepositoryImpl implements GeneroRepositoryQuery{
+    // preparação do criteria
+    @PersistenceContext
+    private EntityManager manager;
+
     @Override
     public Page<Genero> filtrarGenero(GeneroFilter generoFilter, Pageable pageable) {
         return null;
